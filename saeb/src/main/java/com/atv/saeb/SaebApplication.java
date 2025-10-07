1244dffaa6071a6007adcb26e0e3ea17d8dc8a26
viniciusdev00 <vinicius.biancolini.tds24@gmail.com>
2025-10-07 14:12:58 -0300

    1

---
+++ saeb/src/main/java/com/atv/saeb/SaebApplication.java
@@ -0,0 +1,13 @@
+package com.atv.saeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaebApplication.class, args);
+	}
+
+}
---
+++ saeb/src/test/java/com/atv/saeb/SaebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atv.saeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
